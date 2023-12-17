e017e077f7d41c9f6084e02d0528b04cb969fb24
tigarto <helpu1409@gmail.com>
2023-12-17 06:01:25 -0500

    agregando ejemplo

---
+++ test_whisper/test-whisper1.py
@@ -0,0 +1,40 @@
+"""
+Referencias:
+- https://wandb.ai/wandb_fc/gentle-intros/reports/OpenAI-Whisper-How-to-Transcribe-Your-Audio-to-Text-for-Free-with-SRTs-VTTs---VmlldzozNDczNTI0
+
+"""
+
+"""
+Notebook:
+!pip install git+https://github.com/openai/whisper.git
+!sudo apt update && sudo apt install ffmpeg
+"""
+
+import whisper
+import pandas as pd
+import os
+from whisper.utils import get_writer
+
+model = whisper.load_model("base")
+audio = "trabalenguas.mp3"
+result = model.transcribe("trabalenguas.mp3")
+print(result["text"])
+#df = pd.DataFrame(result["segments"])
+#df = pd.DataFrame(result["segments"])[['id','start','end','text']]
+#print(df)
+# save TXT
+
+output_directory = "./"
+
+# Save as a TXT file without any line breaks
+with open("transcription.txt", "w", encoding="utf-8") as txt:
+    txt.write(result["text"])
+
+
+# Save as a TXT file with hard line breaks
+txt_writer = get_writer("txt", output_directory)
+txt_writer(result, audio)
+
+
+# https://github.com/openai/whisper/tree/main/whisper
+
